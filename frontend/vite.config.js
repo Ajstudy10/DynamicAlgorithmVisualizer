e089d21845cbf32dc3c42341a3d2e2215367c448
Omkbest <ajstudy4@gmail.com>
2025-01-25 21:14:58 +0300

    Changed configv2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  allowedHosts: ['joyful-enthusiasm-production.up.railway.app']
+  allowedHosts: ['joyful-enthusiasm-production.up.railway.app'],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['joyful-enthusiasm-production.up.railway.app'],
+  },
 });
